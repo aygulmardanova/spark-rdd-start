fd96b2524811535501b9a43a96703c41a973fdf8
aygulmardanova <mardanovaaigul@mail.ru>
2018-09-29 02:04:30 +0300

    hw 1 write rdd results to file

--- src/main/java/RDDOperations.scala
+++ src/main/java/RDDOperations.scala
@@ -1,9 +1,14 @@
+import org.apache.hadoop.fs.{FileSystem, Path}
 import org.apache.log4j.{Level, Logger}
 import org.apache.spark.{SparkConf, SparkContext}
 
 object RDDOperations {
 
+  val def_filename = "input.txt"
   val take_num_count = 20
+  val outputDir = "output"
+  val partedResult = "/partedResult"
+  val oneFileResult = "/oneFile"
 
   def main(args: Array[String]): Unit = {
 
@@ -16,68 +21,83 @@ object RDDOperations {
 
     val sc = new SparkContext(conf)
 
-    val fileName = if (args.isEmpty) "input.txt" else args(0)
+    val fileName = if (args.isEmpty) def_filename else args(0)
 
     println("File " + fileName + ":")
     val fileRDD = sc.textFile(fileName)
     fileRDD.foreach(println)
 
-    val rows = fileRDD.map(line => line.split(" "))
+    val rows = fileRDD.map(_.split(" "))
     // create the RDD of numbers
-    val numbers = rows.map(row => row.map(s => s.toInt))
+    val numbers = rows.map(_.map(_.toInt))
 
     println("RDD[Array[Int]]")
     println("Array of numbers")
-    numbers.map(number => number.mkString("", " ", "\n")).takeOrdered(take_num_count).foreach(print)
+    numbers.map(_.mkString("", " ", "\n")).takeOrdered(take_num_count).foreach(print)
 
     println("\nSum of numbers of each row:")
-//    val rowsSum = numbers.map(number => number.reduce((a, b) => a + b))
-    val rowsSum = numbers.map(number => number.sum)
+    val rowsSum = numbers.map(_.sum.toString)
     rowsSum.foreach(println)
 
     println("\nSum of numbers for each row (number %5 == 0):")
-    val rowsMult5Sum = numbers.map(number => number.filter(num => num % 5 == 0).sum)
+    val rowsMult5Sum = numbers.map(_.filter(_ % 5 == 0).sum.toString)
     rowsMult5Sum.foreach(println)
 
     println("\nMaximum for each row:")
-    val rowsMax = numbers.map(number => number.max)
+    val rowsMax = numbers.map(_.max.toString)
     rowsMax.foreach(println)
-//    val rowsMax = numbers.map(number => number.reduce((a, b) => if (a >= b) a else b))
-//    rowsMax.map(max => max.toString + " ").takeOrdered(take_num_count).foreach(print)
 
     println("\nMinimum for each row:")
-    val rowsMin = numbers.map(number => number.min)
+    val rowsMin = numbers.map(_.min.toString)
     rowsMin.foreach(println)
 
     println("\nSet of numbers for each row:")
-    val rowsSet = numbers.map(numbersRow => numbersRow.distinct)
-    rowsSet.map(set => set.mkString("", " ", "\n")).takeOrdered(take_num_count).foreach(print)
+    val rowsSet = numbers.map(_.distinct)
+    rowsSet.map(_.mkString("", " ", "\n")).takeOrdered(take_num_count).foreach(print)
 
     println("\nRDD[Int]")
     println("Get an array of all numbers:")
-    var intFlat = fileRDD.flatMap(s => s.split(" ")).map(s => s.toInt)
-    intFlat.map(int => int.toString + " ").foreach(print)
+    var intFlat = fileRDD.flatMap(_.split(" ")).map(_.toInt)
+    intFlat.map(_ + " ").foreach(print)
 
     println("\nSum of all numbers:")
-    var flatSum = intFlat.reduce((a, b) => a + b)
+    var flatSum = intFlat.reduce(_ + _).toString
     println(flatSum)
 
     println("\nSum of all numbers % 5 == 0:")
-    val flatMult5Sum = intFlat.filter(d => d % 5 == 0).reduce((a, b) => a + b)
+    val flatMult5Sum = intFlat.filter(_ % 5 == 0).reduce(_ + _).toString
     println(flatMult5Sum)
 
     println("\nMaximum of all numbers:")
-    val flatMax = intFlat.max
+    val flatMax = intFlat.max.toString
     println(flatMax)
 
     println("\nMinimum of all numbers:")
-    val flatMin = intFlat.min
+    val flatMin = intFlat.min.toString
     println(flatMin)
 
     println("\nSet of all numbers:")
     val intFlatSet = intFlat.distinct()
     intFlatSet.takeOrdered(take_num_count).mkString("", " ", "\n").foreach(print)
 
+    val result =
+      sc.parallelize(Seq("File " + fileName + ":\n")) ++ fileRDD ++
+        sc.parallelize(Seq("\nSum of numbers of each row:\n")) ++ rowsSum ++
+        sc.parallelize(Seq("\nSum of numbers for each row (number %5 == 0):\n")) ++ rowsMult5Sum ++
+        sc.parallelize(Seq("\nMaximum for each row:\n")) ++ rowsMax ++
+        sc.parallelize(Seq("\nMinimum for each row:\n")) ++ rowsMin ++
+        sc.parallelize(Seq("\nSet of numbers for each row:\n")) ++ rowsSet.map(_.mkString("", " ", "")) ++
+        sc.parallelize(Seq("\nGet an array of all numbers:\n")) ++ intFlat.map(_ + " ") ++
+        sc.parallelize(Seq("\nSum of all numbers:\n" + flatSum)) ++
+        sc.parallelize(Seq("\nSum of all numbers % 5 == 0:\n" + flatMult5Sum)) ++
+        sc.parallelize(Seq("\nMaximum of all numbers:\n" + flatMax)) ++
+        sc.parallelize(Seq("\nMinimum of all numbers:\n" + flatMin)) ++
+        sc.parallelize(Seq("\nSet of all numbers:\n")) ++ intFlatSet.map(_ + " ")
+
+    val fs = FileSystem.get(sc.hadoopConfiguration)
+    fs.delete(new Path(outputDir), true)
+    result.saveAsTextFile(outputDir + partedResult)
+    result.coalesce(1).saveAsTextFile(outputDir + oneFileResult)
   }
 
 }
